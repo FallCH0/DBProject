8378e523d0a52466f4bac8e0f43c765cd24164e3
Fall <1621981951@qq.com>
2024-12-04 22:44:14 +0800

    add all tables and pri_key

--- statements.sql
+++ statements.sql
@@ -44,3 +44,65 @@ create table dbo.Company
     Cname    nchar(20) not null,
     Discount float     not null
 )
+/*创建表Airport并设置主键*/
+create table dbo.Airport
+(
+    Aid   char(20)  not null
+        constraint Airport_pk_Aid
+            primary key,
+    Aname NCHAR(20) not null
+)
+/*创建表Crew_Flight并设置主键*/
+create table dbo.Crew_Flight
+(
+    Cfid        char(20)  not null
+        constraint Crew_Flight_Cfid
+            primary key,
+    Cfname      nchar(20) not null,
+    Cfleader_id char(20)  not null,
+    Cftitle     nchar(20) not null,
+    Cflight_id  char(20)  not null
+)
+/*创建表Crew_User并设置主键*/
+create table dbo.Staff
+(
+    Sid         char(20)  not null
+        constraint staff_pk_Sid
+            primary key,
+    Sname       nchar(20) not null,
+    Sleader_id  nchar(20) not null,
+    Stitle      nchar(20) not null,
+    Sairport_id char(20)  not null
+)
+/*创建表Vip并设置主键*/
+create table dbo.Vip
+(
+    Uid char(20) not null
+        constraint Vip_pk_Uid
+            primary key,
+    Cid char(20) not null
+)
+/*创建表Order并设置主键*/
+create table dbo.[Order]
+(
+    Oid   char(20)  not null
+        constraint Order_pk_Oid
+            primary key,
+    Uid   char(20)  not null,
+    Fid   char(20)  not null,
+    Date  datetime  not null,
+    Pay   float     not null,
+    Grade nchar(20) not null
+)
+/*创建表Fli_Date并设置主键*/
+create table dbo.Fli_Date
+(
+    Fid     char(20) not null
+        constraint Fli_Date_pk_Fid
+            primary key,
+    Date    datetime not null,
+    Fir_Num int      not null,
+    Fir_Pri float    not null,
+    Eco_Num int      not null,
+    Eco_Pri int      not null
+)
